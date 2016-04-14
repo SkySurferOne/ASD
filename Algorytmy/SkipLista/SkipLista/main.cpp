20b326d584760e7da273d991c2b867a2852a1721
SkySurferOne <skysurfer95@interia.pl>
2016-04-14 19:52:10 +0200

    sl some proto

--- Algorytmy/SkipLista/SkipLista/main.cpp
+++ Algorytmy/SkipLista/SkipLista/main.cpp
@@ -1,4 +1,49 @@
 #include <iostream>
+#include <ctime>
+#include <cstdlib>
+
+#define MAX_H 4
+
+struct SLNode {
+	int val;
+	SLNode * next[MAX_H];
+};
+
+struct SplitList {
+	SLNode * head;
+	int height;
+};
+
+// 
+int getHeight() {
+	srand(time(NULL));
+	int h = 1;
+
+	while (h < MAX_H && rand() < RAND_MAX/2)
+		h++;
+
+	return h;
+}
+
+//
+void addNode() {
+
+}
+
+//
+int search() {
+
+}
+
+//
+int insert() {
+
+}
+
+// 
+void deleteNode() {
+
+}
 
 int main() {
 	
