7e5a082c5d5a05fd082a63f813e7346620480d51
SkySurferOne <damianhanusiak@gmail.com>
2016-04-15 09:46:45 +0200

    sl wip

--- Algorytmy/SkipLista/SkipLista/main.cpp
+++ Algorytmy/SkipLista/SkipLista/main.cpp
@@ -1,22 +1,28 @@
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
+using namespace std;
 
 #define MAX_H 4
 
 struct SLNode {
-	int val;
+	int val, height;
 	SLNode * next[MAX_H];
+
+	SLNode() {
+		for (int i = 0; i < MAX_H; i++)
+			this->next[i] = NULL;
+	}
 };
 
-struct SplitList {
+struct SkipList {
 	SLNode * head;
 	int height;
 };
 
 // 
 int getHeight() {
-	srand(time(NULL));
+
 	int h = 1;
 
 	while (h < MAX_H && rand() < RAND_MAX/2)
@@ -26,18 +32,58 @@ int getHeight() {
 }
 
 //
-void addNode() {
-
+SLNode * newNode(int key) {
+	SLNode * tmp = new SLNode;
+	tmp->val = key;
+	tmp->height = getHeight();
+	return tmp;
 }
 
 //
-int search() {
+int find(SkipList * skip, int key) {
+	SLNode * it = skip->head;
 
+	for (int i = skip->height - 1; i >= 0; i--) 
+		while (it->next[i] != NULL && key > it->next[i]->val)
+			it = it->next[i];
+
+	return it->next[0] ? it->next[0]->val : -1;
 }
 
 //
-int insert() {
+int insert(SkipList * skip, int key) {
+	SLNode * it = skip->head;
+	SLNode * item = newNode(key);
+	SLNode * fix[MAX_H];
+	memset(fix, NULL, sizeof(SLNode*) * (MAX_H + 1));
 
+	for (int i = skip->height - 1; i >= 0; i--) {
+		while (it->next[i] != NULL && key > it->next[i]->val)
+			it = it->next[i];
+		fix[i] = it;
+	}
+	
+	if (it->next[0] != NULL && it->next[0]->val == key) {
+		return 0;
+	} else {
+
+		if (item->height > skip->height) {
+			for (int i = skip->height; i < item->height; i++) {
+				fix[i] = skip->head;
+			}
+
+			skip->height = item->height;
+		}
+		cout << skip->height << endl;
+
+		for (int i = 0; i < item->height; i++) {
+			item->next[i] = fix[i]->next[i];
+			fix[i]->next[i] = item;
+		}
+	}
+
+	//delete[] * fix;
+	return 1;
 }
 
 // 
@@ -45,8 +91,25 @@ void deleteNode() {
 
 }
 
-int main() {
-	
+// 
+void init(SkipList * skip) {
+	skip->height = 1;
+	SLNode * tmp = new SLNode;
+	tmp->val = -INFINITY;
+	tmp->height = 1;
+	skip->head = tmp;
+}
 
+int main() {
+	srand(time(NULL));
+	SkipList * skip = new SkipList;
+    init(skip);
+	cout << skip->height << endl;
+	insert(skip, 11);
+	/*insert(skip, 22);
+	insert(skip, 33);
+	insert(skip, 9);*/
+
+	system("pause");
 	return 0;
 }
