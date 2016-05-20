956975191b1940336edef91a4cec663184681331
SkySurferOne <damianhanusiak@gmail.com>
2016-05-20 17:54:34 +0200

    dijkstra fixed

--- Algorytmy/Algorytm_Dijkstry/Algorytm_Dijkstry/main.cpp
+++ Algorytmy/Algorytm_Dijkstry/Algorytm_Dijkstry/main.cpp
@@ -13,32 +13,27 @@ int graph[][V_SIZE] = {
 	{ 0, 0, -1, 3, 0, 1 }, // 2
 	{ 0, 3, 0, -1, 0, 0 }, // 3
 	{ 0, 0, 0, 0, -1, 2 }, // 4
-	{ 6, 0, 1, 0, 0, -1 }  // 5  
+	{ 6, 0, 0, 1, 0, -1 }  // 5  
 };
 
 int d[V_SIZE]; // koszty dojœcia
 int p[V_SIZE]; // tablica poprzedników
-int S[V_SIZE];
-int Q[V_SIZE];
-
-
-bool qEmpty() {
-	for (int i = 0; i < V_SIZE; i++) 
-		if (Q[i]) return false;
-	return true;
-}
+bool visited[V_SIZE];
 
 int getMinD() {
-	int min = INT16_MAX;
-	for (int i = 0; i < V_SIZE; i++) {
-		if (d[i] < min) min = i;
-	} 
-	return min;
+	int min_v, i;
+	for (i = 0; i < V_SIZE && visited[i]; i++);
+	min_v = i;
+	for (; i < V_SIZE; i++) {
+		if (!visited[i] && d[i] < d[min_v])
+			min_v = i;
+	}
+	return min_v;
 }
 
 void show(int * tab, int length) {
 	for (int i = 0; i < length; i++) {
-		cout << i << ": " << tab[i] << " ";
+		cout << i << ": " << tab[i] << endl;
 	}
 	cout << endl;
 }
@@ -46,32 +41,26 @@ void show(int * tab, int length) {
 template <size_t size>
 void pathProcess(int(&g)[size][size], int v) {
 	for (int i = 0; i < size; i++) {
-		S[i] = 0;
-		Q[i] = 1;
+		visited[i] = false;
 		d[i] = INT16_MAX;
 		p[i] = -1;
 	}
 	d[v] = 0;
+	int num_vis = 0;
 
-	cout << qEmpty() << endl;
-	cout << getMinD() << endl;
-	show(d, V_SIZE);
-	show(p, V_SIZE);
-
-	while (!qEmpty()) {
+	while (num_vis < size) {
 		int u = getMinD();
-		Q[u] = 0;
-		S[u] = 1;
 		for (int i = 0; i < size; i++) {
-			if (i == u && g[u][i] == 0 && Q[i] == 0) continue;
+			if (g[u][i] <= 0) continue;
 			if (d[i] > d[u] + g[u][i]) {
 				d[i] = d[u] + g[u][i];
 				p[i] = u;
 			}
 		}
+		visited[u] = true;
+		num_vis++;
 	}
 }
-
 
 int main() {
 	int v = 0;
